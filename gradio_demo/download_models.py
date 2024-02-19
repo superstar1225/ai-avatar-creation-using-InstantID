0ba8f8a652943c69f54c229bffa6537d50cd774a
Achilles_Star <paulwilstar1225@gmail.com>
2024-02-19 05:11:48 -0500

    update

--- gradio_demo/download_models.py
+++ gradio_demo/download_models.py
@@ -6,20 +6,20 @@ import os
 hf_hub_download(
     repo_id="InstantX/InstantID",
     filename="ControlNetModel/config.json",
-    local_dir="./checkpoints",
+    local_dir="./checkpoint",
 )
 hf_hub_download(
     repo_id="InstantX/InstantID",
     filename="ControlNetModel/diffusion_pytorch_model.safetensors",
-    local_dir="./checkpoints",
+    local_dir="./checkpoint",
 )
 hf_hub_download(
-    repo_id="InstantX/InstantID", filename="ip-adapter.bin", local_dir="./checkpoints"
+    repo_id="InstantX/InstantID", filename="ip-adapter.bin", local_dir="./checkpoint"
 )
 hf_hub_download(
     repo_id="latent-consistency/lcm-lora-sdxl",
     filename="pytorch_lora_weights.safetensors",
-    local_dir="./checkpoints",
+    local_dir="./checkpoint",
 )
 # download antelopev2
 gdown.download(url="https://drive.google.com/file/d/18wEUfMNohBJ4K3Ly5wpTejPfDzp-8fI8/view?usp=sharing", output="./model/", quiet=False, fuzzy=True)
