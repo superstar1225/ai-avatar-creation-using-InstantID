72f85e0043aa1b7ef0b93c074672369f4cb4efef
Achilles_Star <paulwilstar1225@gmail.com>
2024-02-19 05:22:39 -0500

    update

--- gradio_demo/download_models.py
+++ gradio_demo/download_models.py
@@ -6,22 +6,22 @@ import os
 hf_hub_download(
     repo_id="InstantX/InstantID",
     filename="ControlNetModel/config.json",
-    local_dir="./checkpoint",
+    local_dir="./checkpoints",
 )
 hf_hub_download(
     repo_id="InstantX/InstantID",
     filename="ControlNetModel/diffusion_pytorch_model.safetensors",
-    local_dir="./checkpoint",
+    local_dir="./checkpoints",
 )
 hf_hub_download(
-    repo_id="InstantX/InstantID", filename="ip-adapter.bin", local_dir="./checkpoint"
+    repo_id="InstantX/InstantID", filename="ip-adapter.bin", local_dir="./checkpoints"
 )
 hf_hub_download(
     repo_id="latent-consistency/lcm-lora-sdxl",
     filename="pytorch_lora_weights.safetensors",
-    local_dir="./checkpoint",
+    local_dir="./checkpoints",
 )
 # download antelopev2
-gdown.download(url="https://drive.google.com/file/d/18wEUfMNohBJ4K3Ly5wpTejPfDzp-8fI8/view?usp=sharing", output="./model/", quiet=False, fuzzy=True)
+gdown.download(url="https://drive.google.com/file/d/18wEUfMNohBJ4K3Ly5wpTejPfDzp-8fI8/view?usp=sharing", output="./models/", quiet=False, fuzzy=True)
 # unzip antelopev2.zip
-os.system("unzip ./models/antelopev2.zip -d ./model/")
+os.system("unzip ./models/antelopev2.zip -d ./models/")
